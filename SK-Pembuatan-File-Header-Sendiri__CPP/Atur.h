28f6d20037de0e9652aca7be576ce379b98e234b
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-01-11 21:02:29 +0700

    Add project files.

---
+++ SK-Pembuatan-File-Header-Sendiri__CPP/Atur.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define awal {
+#define akhir }
+#define mulai() main()
+#define cetak cout
+#define tampil cprintf
+#define masuk scanf
+#define jika if
+#define warna textcolor
+#define tahan _getch()
---
+++ SK-Pembuatan-File-Header-Sendiri__CPP/Hitung.cpp
@@ -0,0 +1,28 @@
+#pragma warning(disable:4996)
+/* ---------------------------------- */
+/* program dengan file header sendiri */
+/* ---------------------------------- */
+
+#include <iostream>
+#include <conio.h>
+#include "Atur.h"
+
+using namespace std;
+
+/* Created by Rizky Khapidsyah */
+
+int mulai()
+awal
+int a, b, c;
+
+tampil("\nPROGRAM PENJUMLAHAN\n");
+cout << endl;
+cout << "Masukkan Nilai A = ";
+cin >> a;
+cout << "Masukkan Nilai B = ";
+cin >> b;
+c = a + b;
+cout << "Hasil dari " << a << " + " << b << " = " << c;
+tahan;
+
+akhir
